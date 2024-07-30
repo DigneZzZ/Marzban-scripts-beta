ee0c87a23d352d176631e01eeb4823bc60d70edd
Alexey <50312583+DigneZzZ@users.noreply.github.com>
2024-07-31 01:30:05 +0300

    Update node.sh

--- node.sh
+++ node.sh
@@ -36,7 +36,7 @@ if [ -z "$APP_NAME" ]; then
     APP_NAME="${SCRIPT_NAME%.*}"
 fi
 
-INSTALL_DIR="/root"
+INSTALL_DIR="/opt"
 APP_DIR="$INSTALL_DIR/$APP_NAME"
 DATA_DIR="/var/lib/$APP_NAME"
 DATA_MAIN_DIR="/var/lib/$APP_NAME"
