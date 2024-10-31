a63f5477af664b7776f9e62dfd2babb03038986c
Alexey <50312583+DigneZzZ@users.noreply.github.com>
2024-10-31 23:36:00 +0300

    Update node.sh

--- node.sh
+++ node.sh
@@ -789,7 +789,7 @@ get_xray_core() {
     echo -e "\033[1;32mSelected version $selected_version for installation.\033[0m"
     
     
-    if ! dpkg -s unzip >/dev/null 2>&1; then
+    if ! dpkg -s unzip then
         echo -e "\033[1;33mInstalling required packages...\033[0m"
         detect_os
     install_package unzip
